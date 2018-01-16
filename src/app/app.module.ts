a1de2c2f01071513bd74fd988b84ef6a87153e56
Darwel Quintero <darwel34@gmail.com>
2018-01-15 21:00:16 -0430

    se incluyo un servicio y un nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FechaactualComponent } from './fechaactual/fechaactual.component';
 import { CopyrightComponent } from './copyright/copyright.component';
+import { PruebaService } from './services/prueba.service';
+import { ViewmodelComponent } from './viewmodel/viewmodel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FechaactualComponent,
-    CopyrightComponent
+    CopyrightComponent,
+    ViewmodelComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    PruebaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
