9876750b67ae7e701fb8c31d575e0e8696f53478
Darwel Quintero <darwel34@gmail.com>
2018-01-15 20:05:23 -0430

    se creo el componente fecha actual

---
+++ src/app/fechaactual/fechaactual.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fechaactual',
+  templateUrl: './fechaactual.component.html'
+})
+export class FechaactualComponent {
+  hoy: any = new Date();
+}
